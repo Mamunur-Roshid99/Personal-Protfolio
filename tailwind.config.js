063f8c53d1f7fdebb32edf24284eedaa69fe2d53
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2024-11-24 00:28:55 +0600

    Personal-Protfolio

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    screens: {
+      sm: "480px",
+      // => @media (min-width: 480px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+};
