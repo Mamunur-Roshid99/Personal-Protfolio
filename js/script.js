b3d25fc546d6fe2f3dd995f948f6081527630f9a
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2024-11-27 15:58:29 +0600

    complete project

--- js/script.js
+++ js/script.js
@@ -61,3 +61,42 @@ function darkMode() {
 }
 
 darkMode();
+
+
+
+// popup show\
+function popupShow() {
+  const popup = document.querySelector("#popup");
+  const sendbtn = document.querySelector("#sendbtn");
+  const okbutton = document.querySelector("#okbutton");
+
+  sendbtn.addEventListener("click", () => {
+    
+  setTimeout(() => {
+      popup.classList.add("show");
+    }, 1000);
+  });
+
+  okbutton.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
+}
+
+popupShow();
+
+
+// contact from list
+function contactmessagelist() {
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycbwRDORMtrBXV5AdUaxzUjLRUjXKwxz_Gq8HEZzP3NHxuzt1JJ5RGrPceZUfr1T7iEuo/exec";
+  const form = document.forms["submit-to-google-sheet"];
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then((response) => console.log("Success!", response))
+      .catch((error) => console.error("Error!", error.message));
+  });
+}
+
+contactmessagelist();
