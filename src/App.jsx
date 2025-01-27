2a708a8a3689e26f86a836a85ac4762e063ef2a4
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2025-01-27 23:08:07 +0600

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-800">Hello world!</h1>
+      <p className="">mk</p>
+      <h1 className='tex'>fasdf</h1>
+    </>
+  );
+}
+
+export default App
