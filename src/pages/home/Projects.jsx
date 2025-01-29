074af5a4d13ef780ee1fa3e04a68f6412a3ecffc
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2025-01-29 23:05:00 +0600

    Project

--- src/pages/home/Projects.jsx
+++ src/pages/home/Projects.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+import img1 from "/public/Untitled-1.jpg"
+import img2 from "/public/Untitled-2.jpg"
+import img3 from "/public/Untitled-3.jpg";
+
+import { Link } from 'react-router-dom';
 const Projects = () => {
   return (
     <section>
@@ -16,7 +21,67 @@ const Projects = () => {
           </p>
         </div>
         {/* right */}
-        <div>
+        <div className='flex items-center justify-between w-full gap-10'>
+          {/* project1 */}
+          <div className="group relative overflow-hidden w-[384px] rounded-2xl ">
+            {/* overploy */}
+            <div className="group-hover:opacity-60 group-hover:bg-[#4D5360] absolute w-full h-full z-40 transition-all duration-500"></div>
+            {/* img */}
+            <img src={img1} alt="project1" />
+            {/* title */}
+            <div className="absolute left-7 -bottom-full group-hover:bottom-36 z-50 text-amber-200 transition-all duration-500 font-bold text-2xl">
+              Airbnb Clone Website
+            </div>
+            {/* description */}
+            <div className="absolute left-7 -bottom-full group-hover:bottom-24 text-sm z-50 text-gray-200 transition-all duration-500 w-80">
+              This is an Airbnb Clone Project. Still, we are working on this
+              project. Here, an a...
+            </div>
+            {/* details */}
+            <div className="absolute left-7 -bottom-full group-hover:bottom-[50px] z-50 text-white bg-regal-blue transition-all duration-500 font-semibold text-[15px] rounded-tl-2xl rounded-br-2xl rounded-sm py-[6px] px-[20px] hover:shadow-2xl hover:bg-regal-hover tracking-wide">
+              <Link>Details</Link>
+            </div>
+          </div>
+          {/* project2 */}
+          <div className="group relative overflow-hidden w-[384px] rounded-2xl ">
+            {/* overploy */}
+            <div className="group-hover:opacity-60 group-hover:bg-[#4D5360] absolute w-full h-full z-40 transition-all duration-500"></div>
+            {/* img */}
+            <img src={img2} alt="project2" />
+            {/* title */}
+            <div className="absolute left-7 -bottom-full group-hover:bottom-36 z-50 text-amber-200 transition-all duration-500 font-bold text-2xl">
+              Lending Page Design
+            </div>
+            {/* description */}
+            <div className="absolute left-7 -bottom-full group-hover:bottom-24 text-sm z-50 text-gray-200 transition-all duration-500 w-80">
+              This is an Lending page design project. Still, we are working on
+              this project. Here, an a...
+            </div>
+            {/* details */}
+            <div className="absolute left-7 -bottom-full group-hover:bottom-[50px] z-50 text-white bg-regal-blue transition-all duration-500 font-semibold text-[15px] rounded-tl-2xl rounded-br-2xl rounded-sm py-[6px] px-[20px] hover:shadow-2xl hover:bg-regal-hover tracking-wide">
+              <Link>Details</Link>
+            </div>
+          </div>
+          {/* project3 */}
+          <div className="group relative overflow-hidden w-[384px] rounded-2xl ">
+            {/* overploy */}
+            <div className="group-hover:opacity-60 group-hover:bg-[#4D5360] absolute w-full h-full z-40 transition-all duration-500"></div>
+            {/* img */}
+            <img src={img3} alt="project3" />
+            {/* title */}
+            <div className="absolute left-7 -bottom-full group-hover:bottom-36 z-50 text-amber-200 transition-all duration-500 font-bold text-2xl">
+              Travaling Website
+            </div>
+            {/* description */}
+            <div className="absolute left-7 -bottom-full group-hover:bottom-24 text-sm z-50 text-gray-200 transition-all duration-500 w-80">
+              This is an Travaling Website Project. Still, we are working on
+              this project. Here, an a...
+            </div>
+            {/* details */}
+            <div className="absolute left-7 -bottom-full group-hover:bottom-[50px] z-50 text-white bg-regal-blue transition-all duration-500 font-semibold text-[15px] rounded-tl-2xl rounded-br-2xl rounded-sm py-[6px] px-[20px] hover:shadow-2xl hover:bg-regal-hover tracking-wide">
+              <Link>Details</Link>
+            </div>
+          </div>
         </div>
       </div>
     </section>
