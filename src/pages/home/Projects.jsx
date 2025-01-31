2e2d86824664f339e17fb361e14d0f923005b0ff
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2025-01-31 15:19:53 +0600

    project

--- src/pages/home/Projects.jsx
+++ src/pages/home/Projects.jsx
@@ -7,7 +7,49 @@ import img3 from "/public/Untitled-3.jpg";
 import './project.css'
 
 import { Link } from "react-router-dom";
+
+const ProjectCard = ({ title, image, description }) => {
+  return (
+    <div className="image">
+      <img className="image__img" src={image} alt="website" />
+      <div className="image__overlay image__overlay--primary flex flex-col gap-4">
+        <h3 className="title-color fw-bold text-regal-blue font-bold text-2xl">
+          {title}
+        </h3>
+        <p className="subTitle-color text-sm text-gray-300">
+          {description}
+        </p>
+        <Link className="btn myMenu3 ms-0" to={`/details`}>
+          Details
+        </Link>
+      </div>
+    </div>
+  );
+}
+
 const Projects = () => {
+
+  const projects = [
+    {
+      title: "Airbnb Clone Website",
+      image: img1,
+      description:
+        "This is an Airbnb Clone Project. Still, we are working on this project.",
+    },
+    {
+      title: "Lending Page Design",
+      image: img2,
+      description:
+        "This is a Lending page design project. We are still working on this.",
+    },
+    {
+      title: "Traveling Website",
+      image: img3,
+      description:
+        "This is a Traveling Website Project. More features coming soon.",
+    },
+  ];
+
   return (
     <section>
       {/*  */}
@@ -24,57 +66,11 @@ const Projects = () => {
         </div>
         {/* upper */}
         <div className="flex flex-col lg:flex-row items-center justify-between w-full gap-10">
-          {/* project1 */}
-          <div className="image">
-            <img className="image__img" src={img1} alt="website" />
-            <div className="image__overlay image__overlay--primary">
-              <h3 className="title-color fw-bold">className</h3>
-              <p className="subTitle-color"><details></details>...</p>
-              <Link className="btn myMenu3 ms-0" to={`/details`}>
-                Details
-              </Link>
-            </div>
-          </div>
-          {/* project2 */}
-          <div className="group relative overflow-hidden w-[384px] rounded-2xl ">
-            {/* overploy */}
-            <div className="group-hover:opacity-60 group-hover:bg-[#4D5360] absolute w-full h-full z-40 transition-all duration-500"></div>
-            {/* img */}
-            <img src={img2} alt="project2" />
-            {/* title */}
-            <div className="absolute left-7 -bottom-full group-hover:bottom-36 z-50 text-amber-200 transition-all duration-500 font-bold text-2xl">
-              Lending Page Design
-            </div>
-            {/* description */}
-            <div className="absolute left-7 -bottom-full group-hover:bottom-24 text-sm z-50 text-gray-200 transition-all duration-500 w-80">
-              This is an Lending page design project. Still, we are working on
-              this project. Here, an a...
-            </div>
-            {/* details */}
-            <div className="absolute left-7 -bottom-full group-hover:bottom-[50px] z-50 text-white bg-regal-blue transition-all duration-500 font-semibold text-[15px] rounded-tl-2xl rounded-br-2xl rounded-sm py-[6px] px-[20px] hover:shadow-2xl hover:bg-regal-hover tracking-wide">
-              <Link>Details</Link>
-            </div>
-          </div>
-          {/* project3 */}
-          <div className="group relative overflow-hidden w-[384px] rounded-2xl ">
-            {/* overploy */}
-            <div className="group-hover:opacity-60 group-hover:bg-[#4D5360] absolute w-full h-full z-40 transition-all duration-500"></div>
-            {/* img */}
-            <img src={img3} alt="project3" />
-            {/* title */}
-            <div className="absolute left-7 -bottom-full group-hover:bottom-36 z-50 text-amber-200 transition-all duration-500 font-bold text-2xl">
-              Travaling Website
-            </div>
-            {/* description */}
-            <div className="absolute left-7 -bottom-full group-hover:bottom-24 text-sm z-50 text-gray-200 transition-all duration-500 w-80">
-              This is an Travaling Website Project. Still, we are working on
-              this project. Here, an a...
-            </div>
-            {/* details */}
-            <div className="absolute left-7 -bottom-full group-hover:bottom-[50px] z-50 text-white bg-regal-blue transition-all duration-500 font-semibold text-[15px] rounded-tl-2xl rounded-br-2xl rounded-sm py-[6px] px-[20px] hover:shadow-2xl hover:bg-regal-hover tracking-wide">
-              <Link>Details</Link>
-            </div>
-          </div>
+          {
+            projects.map((project, index) => (
+              <ProjectCard key={index} {...project} />
+            ))
+          }
         </div>
       </div>
     </section>
