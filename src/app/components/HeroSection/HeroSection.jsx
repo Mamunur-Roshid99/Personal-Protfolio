f0edf54b90af595e3f16281aa3929c48a4948627
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2025-08-09 00:45:08 +0600

    .

---
+++ src/app/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+
+import { GoArrowRight } from "react-icons/go";
+import { FiDownload } from "react-icons/fi";
+
+import profile from "../../../../public/assets/profile.png"
+
+const HeroSection = () => {
+  return (
+    <section className="py-12">
+      <div className="flex items-center justify-center text-center px-4 md:px-6 lg:px-12">
+        <div className="flex flex-col items-center justify-center">
+          {/* top */}
+          <div className="flex flex-col gap-4 items-center justify-center">
+            <div className="rounded-full w-32">
+              <Image
+                src={profile}
+                alt="profile"
+                className="rounded-full w-full"
+              />
+            </div>
+            <h4 className="text-xl md:text-2xl mb-3">
+              Hi! I’m Mamunur Roshid Anas 👋🏻
+            </h4>
+          </div>
+          <h1 className="text-3xl lg:text-[60px] lg:max-w-5xl">
+            Mern Stack Developer crafting modern web experiences.
+          </h1>
+          <p className="max-w-2xl mx-auto text-sm md:text-base">
+            I am a Junior Front End Developer, passionate about creating
+            stunning web applications using JavaScript, React and Next.js, eager
+            to collaborate and innovate together.
+          </p>
+          <div className="flex flex-col md:flex-row items-center gap-5 mt-5">
+            <a
+              href="#contact"
+              className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2"
+            >
+              Connect With Me <GoArrowRight />
+            </a>
+            <a
+              href="https://drive.google.com/file/d/16cDrypI3mcVuIZzeeSABuVtsDFrdjeIz/view?usp=sharing"
+              target="_blank"
+              className="px-10 py-3 border border-[#A7A7A7] rounded-full flex items-center gap-2"
+            >
+              My Resume <FiDownload />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
---
+++ src/app/layout/Navbar.jsx
@@ -0,0 +1,88 @@
+import Image from "next/image";
+
+import { CiDark } from "react-icons/ci";
+import { GoArrowUpRight } from "react-icons/go";
+
+import MenubarDrawer from "../components/MenubarDrawer";
+
+import image from "../../../public/assets/navbar.png"
+
+const Navbar = () => {
+  return (
+    <header>
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+        <Image src={image} alt="image" className="w-full" />
+      </div>
+      <nav className="max-w-7xl px-4 md:px-6 lg:px-12 mx-auto py-5">
+        <div className="flex items-center justify-between">
+          {/* logo */}
+          <div className="flex text-2xl lg:text-4xl text-[#1E1E1E] font_outfit font-semibold">
+            <h5>Mamun</h5>
+            <span className="text-[#EC1552]">.</span>
+          </div>
+          {/* links */}
+          <ul className="hidden lg:flex items-center gap-8 bg-white/50 rounded-full px-12 py-3 shadow-xs">
+            <li>
+              <a
+                href="#home"
+                className="hover:text-blue-600 transition-colors duration-300"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about"
+                className="hover:text-blue-600 transition-colors duration-300"
+              >
+                About me
+              </a>
+            </li>
+            <li>
+              <a
+                href="#skill"
+                className="hover:text-blue-600 transition-colors duration-300"
+              >
+                Skills
+              </a>
+            </li>
+            <li>
+              <a
+                href="#work"
+                className="hover:text-blue-600 transition-colors duration-300"
+              >
+                My work
+              </a>
+            </li>
+            <li>
+              <a
+                href="#contact"
+                className="hover:text-blue-600 transition-colors duration-300"
+              >
+                Contact Me
+              </a>
+            </li>
+          </ul>
+          {/* dark & menu icon */}
+          <div className="flex gap-5 lg:items-center">
+            {/* dark */}
+            <div className="text-2xl cursor-pointer">
+              <CiDark />
+            </div>
+            {/* menu */}
+            <div className="text-xl lg:hidden">
+              <MenubarDrawer />
+            </div>
+            {/* contact button */}
+            <div className="hidden border border-[#A7A7A7] px-6 py-2 rounded-full lg:flex items-center gap-2 text-[#4D4D4D] cursor-pointer font_outfit hover:scale-105 duration-500">
+              <p>Connect</p>
+              <GoArrowUpRight className="text-lg" />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
